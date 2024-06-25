023bb549ee8d872763410da5a4497ee2e0fac964
Blendear <tobiascibis@gmail.com>
2024-06-25 14:19:05 +0200

    feat: Add german locale translations data

---
+++ public/locales/de.js
@@ -0,0 +1,30 @@
+export const de = {
+  footer: {
+    home: "Startseite",
+    aboutUs: "Über uns",
+    contact: "Kontakt",
+    investitions: "Investitionen",
+    offers: "Angebote",
+
+    addressStreet: "Al. Jana Pawła II 58 B/7",
+    addressCode: "47-232",
+    addressCity: "Kędzierzyn-Koźle",
+    addressCountry: "Polen",
+
+    phoneLabel: "Tel",
+    emailLabel: "Email",
+    krsLabel: "KRS",
+    nipLabel: "NIP",
+    regonLabel: "REGON",
+  },
+
+  aboutUs: {},
+
+  contact: {},
+
+  home: {},
+
+  investitions: {},
+
+  offers: {},
+};
