dfa9d178021a6aeb8c097116b9b58600c3ee61b6
Blendear <tobiascibis@gmail.com>
2024-06-28 12:15:26 +0200

    feat(offered-real-estates): Add raw starter files

---
+++ src/features/offered-real-estates/components/AClassicComponent.tsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { variables } from "@/styles/emotion-css/variables";
+import { universalCss } from "@/styles/emotion-css/universal";
+import { colors } from "@/styles/emotion-css/colors";
+import { OneTwoThreeFourWordComponentNameProps } from "@/types";
+
+const OneTwoTFWCNCss = {
+  container: css({}),
+};
+
+export const OneTwoThreeFourWordComponentName =
+  ({}: OneTwoThreeFourWordComponentNameProps) => {
+    return <div>OneTwoThreeFourWordComponentName</div>;
+  };
