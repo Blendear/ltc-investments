1dcb291a4bde7576e4fd2e8900bdf5b91d180a96
Blendear <tobiascibis@gmail.com>
2024-07-26 10:49:50 +0200

    chore(offers): Add real consultant image

--- src/features/offered-real-estates/data/consultants.tsx
+++ src/features/offered-real-estates/data/consultants.tsx
@@ -4,7 +4,7 @@ export const consultants: ConsultantType[] = [
   {
     name: "lukas",
     surname: "cibis",
-    imagePath: "https://fwcdn.pl/nph/1985708/2023/47925_1.11.jpg",
+    imagePath: "/images/offers/Lukas.jpg",
     phoneNumber: "+48 604 513 082",
     email: "l.cibis@gmail.com",
   },
