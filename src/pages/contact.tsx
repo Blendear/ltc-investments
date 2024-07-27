caffc71a2d53414372d7e3dbdaa05094341c3d29
Blendear <tobiascibis@gmail.com>
2024-07-27 12:56:44 +0200

    style(contact page): Add styling to page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -10,23 +10,75 @@ import { MdOutlineLocalPhone } from "react-icons/md";
 import ImageWithWrapper from "@/components/ImageWithWrapper";
 
 const contactCss = {
-  container: css({}),
+  container: css({
+    width: "100%",
+    display: "grid",
+    gap: variables.gap.lg,
+    justifyItems: "center",
+  }),
 
-  weAreHere: {
-    container: css({}),
+  header: css({
+    textAlign: "center",
 
-    header: css({}),
+    "@media (max-width: 580px)": {
+      width: "25rem",
+    },
+  }),
 
-    map: {
-      container: css({ width: "100%" }),
+  map: css({
+    width: "clamp(50px, 80vw, 600px)",
+    aspectRatio: "16/9",
 
-      image: css({
-        aspectRatio: "16/9",
-      }),
+    "&:hover": css({
+      filter: "drop-shadow(0 0 0.5rem rgba(0, 0, 0, 0.5)) saturate(0.8)",
+      transition: "filter 0.5s",
+    }),
+
+    "& > span": {
+      borderRadius: variables.borderRadius.md,
+    },
+  }),
+
+  contantInfo: {
+    display: "grid",
+    gap: variables.gap.md,
+    justifyItems: "center",
+
+    "@media (min-width: 650px)": {
+      gridTemplateColumns: "repeat(2, 1fr)",
+    },
+
+    "& > section": {
+      width: "26rem",
+      display: "grid",
+      gridTemplateColumns: "max-content 1fr",
+      gap: variables.gap.md,
+      alignItems: "center",
+
+      "& svg": {
+        color: `rgb(${colors.secondaryLight})`,
+      },
+
+      "& > div:nth-of-type(1)": {
+        ...universalCss.wrapperWithCenteredSvg("4rem", "80%"),
+      },
+
+      "& > div:nth-of-type(2)": {
+        display: "grid",
+        gap: variables.gap.sm,
+      },
+
+      "& a": {
+        "&:hover": {
+          opacity: 0.7,
+        },
+      },
+    },
+
+    "& > section:nth-of-type(1)": {
+      gridRow: "1 / 4",
     },
   },
-
-  svgWithDividerAndData: css({}),
 };
 
 const Contact = () => {
@@ -34,74 +86,63 @@ const Contact = () => {
 
   return (
     <article css={contactCss.container}>
-      <section css={contactCss.weAreHere.container}>
-        <h1>{t.header}</h1>
-        <a
-          href="https://www.google.com/maps/place/Aleja+Jana+Paw%C5%82a+II+58%2F7,+47-232+K%C4%99dzierzyn-Ko%C5%BAle/@50.3521152,18.2126408,14z/data=!4m5!3m4!1s0x47111116a621f50f:0xd3e91c2074b54a6e!8m2!3d50.3516983!4d18.2348327?hl=pl-PL&entry=ttu"
-          css={contactCss.weAreHere.map.container}
-        >
+      <h1 css={contactCss.header}>{t.header}</h1>
+      <section>
+        <a href="https://www.google.com/maps/place/Aleja+Jana+Paw%C5%82a+II+58%2F7,+47-232+K%C4%99dzierzyn-Ko%C5%BAle/@50.3521152,18.2126408,14z/data=!4m5!3m4!1s0x47111116a621f50f:0xd3e91c2074b54a6e!8m2!3d50.3516983!4d18.2348327?hl=pl-PL&entry=ttu">
           <ImageWithWrapper
             src="/images/contact/map.png"
             alt="A map with a location of the company"
-            wrapperCss={contactCss.weAreHere.map.image}
+            wrapperCss={contactCss.map}
           />
         </a>
       </section>
 
-      <section css={contactCss.svgWithDividerAndData}>
-        <div>
-          <IoIosPin />
-        </div>
-        <div>
-          <p> {t.addressStreet},</p>
-          <p> {t.addressCode},</p>
-          <p> {t.addressCity},</p>
-          <p> {t.addressCountry}</p>
-          <p>
-            {t.nipLabel}: <span>7543070445</span>
-          </p>
-          <p>
-            {t.krsLabel}: <span>0001059206</span>
-          </p>
-          <p>
-            {t.regonLabel}: <span>161512181</span>
-          </p>
-        </div>
-      </section>
+      <section css={contactCss.contantInfo}>
+        <section>
+          <div>
+            <IoIosPin />
+          </div>
+          <div>
+            <p>{t.addressStreet}</p>
+            <p>
+              {t.addressCode}, {t.addressCity}
+            </p>
+            <p>
+              {t.nipLabel}: <span>7543070445</span>
+            </p>
+            <p>
+              {t.krsLabel}: <span>0001059206</span>
+            </p>
+            <p>
+              {t.regonLabel}: <span>161512181</span>
+            </p>
+          </div>
+        </section>
 
-      <section css={contactCss.svgWithDividerAndData}>
-        <div>
-          <IoMdTime />
-        </div>
-        <div>
-          <p>08:00 - 16:00</p>
-        </div>
-      </section>
+        <section>
+          <div>
+            <MdOutlineLocalPhone />
+          </div>
+          <div>
+            <a href={`tel:+48 604 513 082`}>+48 604 513 082</a>
+          </div>
+        </section>
 
-      <section css={contactCss.svgWithDividerAndData}>
-        <div>
-          <MdOutlineLocalPhone />
-        </div>
-        <div>
-          <p>
-            {t.phoneLabel}:{" "}
-            <span>
-              <a href={`tel:+48 604 513 082`}>+48 604 513 082</a>
-            </span>
-          </p>
-        </div>
-      </section>
+        <section>
+          <div>
+            <IoMdTime />
+          </div>
+          <div>
+            <p>08:00 - 16:00</p>
+          </div>
+        </section>
 
-      <section css={contactCss.svgWithDividerAndData}>
-        <div>
-          <IoMailOutline />
-        </div>
-        <p>
-          {t.emailLabel}:{" "}
-          <span>
-            <a href={`mailto:l.cibis@gmail.com`}>l.cibis@gmail.com</a>
-          </span>
-        </p>
+        <section>
+          <div>
+            <IoMailOutline />
+          </div>
+          <a href={`mailto:l.cibis@gmail.com`}>l.cibis@gmail.com</a>
+        </section>
       </section>
     </article>
   );
